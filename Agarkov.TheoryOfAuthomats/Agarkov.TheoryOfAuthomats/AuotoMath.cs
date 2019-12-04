143a7dec9d4809aad72d068563dbf3fa6373a365
student <student@A303_52.vt2.kstu>
2019-12-04 14:35:01 +0300

    исходники теории автоматов

---
+++ Agarkov.TheoryOfAuthomats/Agarkov.TheoryOfAuthomats/AuotoMath.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agarkov.TheoryOfAuthomats
+{
+    /// <summary>
+    /// Конечный автомат
+    /// </summary>
+    class AuotoMath
+    {
+        /// <summary>
+        /// Алфавит автомата
+        /// </summary>
+        private List<string> _alphabet;
+
+        /// <summary>
+        /// Закодированный алфавит
+        /// </summary>
+        private Dictionary<string, int> _encodedAlphabet;
+
+        /// <summary>
+        /// Входной текст
+        /// </summary>
+        public string Text { get; private set; }
+
+        /// <summary>
+        /// Состояние ошибки
+        /// </summary>
+        public int ErrorState { get; private set; }
+
+        /// <summary>
+        /// Перегруженный конструктор
+        /// </summary>
+        /// <param name="alphabet">Входной алфавит</param>
+        /// <param name="inputText">Входной текст</param>
+        public AuotoMath(List<string> alphabet, string inputText)
+        {
+            if (alphabet == null)
+            {
+                throw new Exception("Алфавит задан некорректно");
+            }
+
+            if (String.IsNullOrEmpty(inputText))
+            {
+                throw new Exception("Текст задан некорректно");
+            }
+
+            _alphabet = alphabet;
+            Text = inputText;
+            _encodedAlphabet = new Dictionary<string, int>();
+        }
+
+        /// <summary>
+        /// Закодировать входной алфавит
+        /// </summary>
+        public void EncodeAlphabet()
+        {
+            for(var i = 0; i < _alphabet.Count; i++)
+            {
+                _encodedAlphabet.Add(_alphabet.ElementAt(i), i); 
+            }
+
+            ErrorState = _alphabet.Count;
+        }
+
+        public List<int> EncodeText()
+        {
+            var encodedInts = new List<int>();
+
+            foreach(var symbol in Text)
+            {
+                var temp = _encodedAlphabet.FirstOrDefault(s => s.Key == symbol.ToString());
+                if (temp.Key == null)
+                {
+                    throw new Exception("Ошибочное состояние");
+                }
+                encodedInts.Add(temp.Value);
+            }
+
+            return encodedInts;
+
+        }
+    }
+}
---
+++ Agarkov.TheoryOfAuthomats/Agarkov.TheoryOfAuthomats/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agarkov.TheoryOfAuthomats
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var alphabeth = new List<string> { "a", "c", "f", "e", "m", "n", "k" };
+                var text = "facamnekfca";
+
+                var a = new AuotoMath(alphabeth, text);
+
+                a.EncodeAlphabet();
+                var b = a.EncodeText();
+
+                foreach(var item in b)
+                {
+                    Console.Write($"{item} ");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+        }
+    }
+}
